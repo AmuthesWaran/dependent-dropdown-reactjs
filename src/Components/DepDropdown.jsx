ee4e4b871cae3058a9f8e36334ac6bae012243f1
Amutheswaran <amutheswaran@outlook.com>
2023-02-16 05:43:16 +0530

    done

---
+++ src/Components/DepDropdown.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const DepDropdown = () => {
+
+    const dropdownList = ['Select', 'Fruits', 'Vegetables', 'Colors']
+
+    const dependentList = {
+        Select: ['.....'],
+        Fruits: ['.....', 'Apple', 'Mango', 'Banana', 'Grapes', 'Oranges'],
+        Vegetables: ['.....', 'Carrot', 'Tomato', 'Onion', 'Potato', 'Radish'],
+        Colors: ['.....', 'Red', 'Blue', 'Green', 'Yellow', 'Pink']
+    }
+
+    const [selectedDropdownList, setDropdownSelectedList] = useState('Select')
+    const [depSelectedList, setDepSelectedList] = useState('')
+    console.log(selectedDropdownList)
+    console.log(depSelectedList)
+
+
+
+    return (
+        <Container fluid >
+            <Row className="mt-4 pt-4">
+                <Col lg={3} md={6} sm={6} className="p-3 m-auto shadow-lg rounded-lg bg-light">
+                    <Row>
+                        <Col>
+                            <select onChange={e => setDropdownSelectedList(e.target.value)}>
+                                {
+                                    dropdownList.map(list => {
+                                        return <option key={list} >{list}</option>
+                                    })
+                                }
+                            </select>
+                        </Col>
+                        <Col>
+                            {selectedDropdownList && <select onChange={e => setDepSelectedList(e.target.value)} >
+                                {
+                                    dependentList[selectedDropdownList].map(dlist => {
+                                        return <option key={dlist}>{dlist}</option>
+                                    })
+                                }
+
+                            </select>
+                            }
+                        </Col>
+
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+
+
+    )
+}
+
+export default DepDropdown
